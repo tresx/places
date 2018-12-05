d2b771bc9164f366287118ed098c88c32d0cc510
efficientoranges <efficientoranges@gmail.com>
2018-12-05 10:31:46 +0000

    Update config to use environment variables from config.env file.

--- places/__init__.py
+++ places/__init__.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 
 from flask import Flask
 
@@ -6,14 +7,20 @@ from flask import Flask
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    app.config.update(
         SECRET_KEY='dev',
-        DATABASE=os.environ['DATABASE_URL'],
+        DATABASE=os.getenv('DATABASE_URL')
     )
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        load_dotenv(os.path.join(basedir, 'config.env'))
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            API_KEY=os.getenv('API_KEY')
+        )
+
     else:
         # Load the test config if passed in
         app.config.from_mapping(test_config)
